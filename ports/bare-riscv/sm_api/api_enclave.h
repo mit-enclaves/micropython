119e10ccec29ba5dde2a473ccbf6ebf7e39317c2
Jules Drean <drean@mit.edu>
2023-04-17 20:26:05 +0000

    Sign output

--- ports/bare-riscv/sm_api/api_enclave.h
+++ ports/bare-riscv/sm_api/api_enclave.h
@@ -8,12 +8,12 @@ static inline api_result_t sm_exit_enclave(void) {
    return SM_API_CALL(SM_ENCLAVE_EXIT, 0, 0, 0, 0, 0, 0, 0);
 }
 
-static inline api_result_t sm_get_attestation_key(uintptr_t phys_addr) {
-   return SM_API_CALL(SM_GET_ATTESTATION_KEY, phys_addr, 0, 0, 0, 0, 0, 0);
+static inline api_result_t sm_enclave_get_keys (void *addr_m, void *addr_pk, void *addr_sk, void *addr_a) {
+  return SM_API_CALL(SM_ENCLAVE_GET_KEYS, addr_m, addr_pk, addr_sk, addr_a, 0, 0, 0);
 }
 
-static inline api_result_t sm_get_public_field (public_field_t field, phys_ptr_t phys_addr) {
-   return SM_API_CALL(SM_GET_PUBLIC_FIELD, field, phys_addr, 0, 0, 0, 0, 0);
+static inline api_result_t sm_get_public_field (public_field_t field, void *addr) {
+   return SM_API_CALL(SM_GET_PUBLIC_FIELD, field, addr, 0, 0, 0, 0, 0);
 }
 
 static inline api_result_t sm_region_block(region_id_t id) {
@@ -28,13 +28,12 @@ static inline api_result_t sm_mail_accept(mailbox_id_t mailbox_id, enclave_id_t 
    return SM_API_CALL(SM_MAIL_ACCEPT, mailbox_id, expected_sender, 0, 0, 0, 0, 0);
 }
 
-static inline api_result_t sm_mail_receive(mailbox_id_t mailbox_id, phys_ptr_t out_message, phys_ptr_t out_sender_measurement) {
+static inline api_result_t sm_mail_receive(mailbox_id_t mailbox_id, void *out_message, void *out_sender_measurement) {
    return SM_API_CALL(SM_MAIL_RECEIVE, mailbox_id, out_message, out_sender_measurement, 0, 0, 0, 0);
 }
 
-static inline api_result_t sm_mail_send(enclave_id_t enclave_id, mailbox_id_t mailbox_id,
-      uintptr_t phys_addr) {
-   return SM_API_CALL(SM_MAIL_SEND, enclave_id, mailbox_id, phys_addr, 0, 0, 0, 0);
+static inline api_result_t sm_mail_send(enclave_id_t enclave_id, mailbox_id_t mailbox_id, void * addr) {
+   return SM_API_CALL(SM_MAIL_SEND, enclave_id, mailbox_id, addr, 0, 0, 0, 0);
 }
 
 #endif // API_ENCLAVE_H
--- ports/bare-riscv/system.c
+++ ports/bare-riscv/system.c
@@ -26,8 +26,9 @@
 
 #include <stdint.h>
 #include <string.h>
+#include <api_enclave.h>
 #include "py/mpconfig.h"
-#include "api_enclave.h"
+#include "cryptography.h"
 
 extern uint32_t _estack, _sidata, _sdata, _edata, _sbss, _ebss;
 
@@ -35,8 +36,18 @@ void Reset_Handler(void) __attribute__((naked));
 void bare_main(void);
 
 static void enclave_init(void);
+void mp_hal_stdout_tx_strn(const char *str, mp_uint_t len);
 
 static char* console_buf_idx;
+
+hash_t measurement;
+public_key_t pk;
+secret_key_t sk;
+signature_t attestation;
+
+hash_context_t output_h_ctxt;
+
+const char hex_map[] = "0123456789ABCDEF";
 
 // The CPU runs this function when entering the enclave.
 void enclave_entry(uintptr_t console_buf) {
@@ -52,6 +63,23 @@ void enclave_entry(uintptr_t console_buf) {
     // Now that there is a basic system up and running, call the main application code.
     bare_main();
 
+    // Sign the output hash and print the signature
+    hash_t output_h;
+    hash_finalize(&output_h_ctxt, &output_h);
+
+    signature_t signature;
+    sign(&output_h, sizeof(hash_t), &pk, &sk, &signature);
+
+    int len = sizeof(signature_t);
+    char signature_str[2 * len + 1];
+    for(int i=0; i < len; i++) {
+      signature_str[i * 2] = hex_map[signature.bytes[i] >> 4];
+      signature_str[i * 2 + 1] = hex_map[signature.bytes[i] & 0x0F];
+    }
+    signature_str[len * 2] = '\n';
+
+    mp_hal_stdout_tx_strn(signature_str, len * 2 + 1);
+
     sm_exit_enclave(); 
     // This function must not return.
     for (;;) {
@@ -60,9 +88,16 @@ void enclave_entry(uintptr_t console_buf) {
 
 // Set up the ENCLAVE.
 static void enclave_init(void) {
+  api_result_t res;
+  do{
+    res = sm_enclave_get_keys(&measurement, &pk, &sk, &attestation);
+  } while(res != MONITOR_OK);
+
+  hash_init(&output_h_ctxt);
 }
 
 static inline uintptr_t console_putchar(uint8_t c) {
+  hash_extend(&output_h_ctxt, &c, sizeof(uint8_t));
   *console_buf_idx++ = c;
   return 0x0;
 }
